d42de992d2139434bf0e008439f62a330062f5ee
Lucia (Seokyung Kim) <0617lucia@gmail.com>
2024-03-17 12:59:16 +0700

    feat: ClaimProcessManager Interface defined & SimpleClaimProcessManager with operations

---
+++ ClaimProcessManager.java
@@ -0,0 +1,9 @@
+import java.util.List;
+
+interface ClaimProcessManager {
+    void add(Claim claim);
+    void update(Claim claim);
+    void delete(String claimId);
+    Claim getOne(String claimId);
+    List<Claim> getAll();
+}

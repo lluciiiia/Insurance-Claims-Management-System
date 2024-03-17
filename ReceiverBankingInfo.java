58764eddbd2362508574ff9de395f1d2fce9f3be
Lucia (Seokyung Kim) <0617lucia@gmail.com>
2024-03-17 13:01:07 +0700

    feat: ReceiverBankingInfo class defined with getters & setters

---
+++ ReceiverBankingInfo.java
@@ -0,0 +1,36 @@
+public class ReceiverBankingInfo {
+    private String bank;
+    private String name;
+    private String number;
+
+    public ReceiverBankingInfo(String bank, String name, String number) {
+        this.bank = bank;
+        this.name = name;
+        this.number = number;
+    }
+
+    // Getters and setters
+    public String getBank() {
+        return bank;
+    }
+
+    public void setBank(String bank) {
+        this.bank = bank;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+}

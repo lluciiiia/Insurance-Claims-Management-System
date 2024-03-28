2f85c7ef5635d0a26da498a0ecf485ed9e67333a
Lucia (Seokyung Kim) <0617lucia@gmail.com>
2024-03-28 16:06:33 +0700

    refactor!: separation of MenuManager

--- src/Application.java
+++ src/Application.java
@@ -2,7 +2,7 @@ package src;
 
 import src.ClaimProcessManager.ClaimProcessManager;
 import src.ClaimProcessManager.ClaimProcessManagerImpl;
-import src.AdminOperationManager.*;
+import src.MenuManager;
 import src.FileManager.FileManager;
 import src.FileManager.FileManagerImpl;
 import src.domain.Claim;
@@ -25,65 +25,8 @@ public class Application {
     }
 
     public void start() {
-        displayMenu();
-        handleUserInput();
-    }
-
-    public void displayMenu() {
-        System.out.println("Welcome to the Insurance Claims Management System!");
-        System.out.println("1. Add Claim");
-        System.out.println("2. Update Claim");
-        System.out.println("3. Delete Claim");
-        System.out.println("4. Get Claim Details");
-        System.out.println("5. Get All Claims");
-        System.out.println("6. Exit");
-    }
-
-    public void handleUserInput() {
-        AdminOperationManager adminManager = new AdminOperationManagerImpl();
-        Scanner scanner = new Scanner(System.in);
-        int choice;
-
-        do {
-            System.out.print("Enter your choice: ");
-            choice = scanner.nextInt();
-
-            switch (choice) {
-                case 1:
-                    adminManager.handleAddClaim(claimProcessManager, dataMap);
-                    break;
-                case 2:
-                    adminManager.handleUpdateClaim(claimProcessManager, dataMap);
-                    break;
-                case 3:
-                    adminManager.handleDeleteClaim(claimProcessManager);
-                    break;
-                case 4:
-                    adminManager.handleGetClaim(claimProcessManager);
-                    break;
-                case 5:
-                    adminManager.handleGetAllClaims(claimProcessManager);
-                    break;
-                case 6:
-                    System.out.println("Exiting...");
-                    saveDataAndExit();
-                    break;
-                default:
-                    System.out.println("Invalid choice. Please enter a number between 1 and 6.");
-            }
-        } while (choice != 6);
-
-        scanner.close();
-    }
-
-    private void saveDataAndExit() {
-        try {
-            fileManager.saveFiles((HashMap<String, HashMap<String, ?>>) dataMap);
-        } catch (IOException e) {
-            System.out.println("An error occurred while saving data: " + e.getMessage());
-        }
-
-        System.out.println("Exiting...");
+        MenuManager.displayMenu();
+        MenuManager.handleUserInput(claimProcessManager, dataMap, fileManager);
     }
 
     public static void main(String[] args) throws IOException {
---
+++ src/MenuManager.java
@@ -0,0 +1,71 @@
+package src;
+
+import src.AdminOperationManager.AdminOperationManager;
+import src.AdminOperationManager.AdminOperationManagerImpl;
+import src.ClaimProcessManager.ClaimProcessManager;
+import src.FileManager.FileManager;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Scanner;
+
+/*
+ * @author <Seokyung Kim - s3939114>
+ */
+public class MenuManager {
+
+    public static void displayMenu() {
+        System.out.println("Welcome to the Insurance Claims Management System!");
+        System.out.println("1. Add Claim");
+        System.out.println("2. Update Claim");
+        System.out.println("3. Delete Claim");
+        System.out.println("4. Get Claim Details");
+        System.out.println("5. Get All Claims");
+        System.out.println("6. Exit");
+    }
+
+    public static void handleUserInput(ClaimProcessManager claimProcessManager, HashMap<String, HashMap<String, ?>> dataMap, FileManager fileManager) {
+        AdminOperationManager adminManager = new AdminOperationManagerImpl();
+        Scanner scanner = new Scanner(System.in);
+        int choice;
+
+        do {
+            System.out.print("Enter your choice: ");
+            choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    adminManager.handleAddClaim(claimProcessManager, dataMap);
+                    break;
+                case 2:
+                    adminManager.handleUpdateClaim(claimProcessManager, dataMap);
+                    break;
+                case 3:
+                    adminManager.handleDeleteClaim(claimProcessManager);
+                    break;
+                case 4:
+                    adminManager.handleGetClaim(claimProcessManager);
+                    break;
+                case 5:
+                    adminManager.handleGetAllClaims(claimProcessManager);
+                    break;
+                case 6:
+                    System.out.println("Exiting...");
+
+                    try {
+                        fileManager.saveFiles((HashMap<String, HashMap<String, ?>>) dataMap);
+                    } catch (IOException e) {
+                        System.out.println("An error occurred while saving data: " + e.getMessage());
+                    }
+
+                    System.out.println("System ended.");
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please enter a number between 1 and 6.");
+            }
+        } while (choice != 6);
+
+        scanner.close();
+    }
+
+}
